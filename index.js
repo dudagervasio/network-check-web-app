55c082d93f66120413adde7af27d29645aaf6110
Duda Gervásio <dudagervasio@yahoo.com.br>
2022-09-08 19:21:08 -0300

    counter

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80;
 
+const obj = {
+	error: false,
+	count: 0
+};
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/counter', (req, res) => {
+  res.send( obj );
 })
 
 app.listen(port, (err) => {
@@ -13,3 +23,8 @@ app.listen(port, (err) => {
   console.log(`Example app listening on port ${port}`)
 
 })
+
+setInterval( () => {
+
+	count++;
+}, 1000);
