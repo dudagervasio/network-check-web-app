8642413a602a3d32bf6557957101a931f1aff333
Duda Gervásio <dudagervasio@yahoo.com.br>
2022-09-08 19:16:57 -0300

    PORT 80

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+
   console.log(`Example app listening on port ${port}`)
+
 })
