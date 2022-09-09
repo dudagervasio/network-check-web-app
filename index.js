cf0c21698cfbfff25cd15aca7b033cf6d0e6fc5d
Duda Gervásio <dudagervasio@yahoo.com.br>
2022-09-08 22:52:25 -0300

    mail

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
+const { sendMail } = require('./src/services/mailService');
 const app = express()
 const port = process.env.PORT || 80;
 
-const obj = {
+const status = {
 	error: false,
 	count: 0
 };
@@ -18,11 +19,37 @@ app.get('/counter', (req, res) => {
   res.send( obj );
 })
 
+app.get('/sendMail', async (req, res) => {
+
+	try{
+
+		const info = await sendMail('teste');
+
+		res.send( {
+			success: true,
+			info
+		} );
+
+	}catch(err){
+
+		console.log(err);
+
+		res.send( {
+			success: false,
+			err
+		} );
+
+	}
+	
+})
+
 app.listen(port, (err) => {
 
   console.log(`Example app listening on port ${port}`)
 
 })
+
+
 
 setInterval( () => {
 
---
+++ src/services/mailService.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendMail(text) {
+
+/* 	let transporter = nodemailer.createTransport({
+		host: "smtp.mailtrap.io",
+		port: 2525,
+		auth: {
+			user: "650700a5a63290",
+			pass: "97b9aa221f2bc4"
+		}
+	});
+ */
+	let transporter = nodemailer.createTransport({
+		host: "mail.hidratapharma.com.br",
+		port: 465,
+		secure: true, 
+		auth: {
+			user: "contato@hidrtapharma.com.br",
+			pass: "@hidcontato01"
+		}
+	});
+
+    // send mail with defined transport object
+	let info = await transporter.sendMail({
+		from: '"HIDRATA TI" <contato@hidratapharma.com.br>', // sender address
+		to: "luis.eduardo@hidratapharma.com.br, dudagervasio@yahoo.com.br", // list of receivers
+		subject: "NETWORK PROBLEM", // Subject line
+		text, // plain text body
+		html: "<b>" + text + "</b>", // html body
+	  });
+	
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  return info;
+
+}
+
+//sendMail('bla bla').catch((err) => { console.log('ERRO MESMO'); console.error(err)});
+
+module.exports = { sendMail };
